55ca2d152f79aa631f2b95aab4ad020361bafb83
Jonah Williams <jonah@carbonfive.com>
2011-07-16 20:25:14 -0700

    Create stub storify service

---
+++ storied/StorifyService.h
@@ -0,0 +1,16 @@
+//
+//  StorifyService.h
+//  storied
+//
+//  Created by Jonah Williams on 7/16/11.
+//  Copyright 2011 Carbon Five. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+
+@interface StorifyService : NSObject {
+    
+}
+
+@end
---
+++ storied/StorifyService.m
@@ -0,0 +1,14 @@
+//
+//  StorifyService.m
+//  storied
+//
+//  Created by Jonah Williams on 7/16/11.
+//  Copyright 2011 Carbon Five. All rights reserved.
+//
+
+#import "StorifyService.h"
+
+
+@implementation StorifyService
+
+@end
